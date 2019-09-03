9b5d590b8390dc273efbca58af062126281e584b
handsome.mofokeng <handsomem23@hotmail.com>
2019-09-03 14:50:16 +0200

    Removed Portfolio emum

--- app/src/main/java/com/example/srcvotingapp/Portfolio.java
+++
@@ -1,8 +0,0 @@
-package com.example.srcvotingapp;
-
-public enum Portfolio {
-
-    President,  DeputyPresident, SecretaryGeneral, FinancialOfficer, ConstitutionalAndLegalSffairs,
-    SportsOfficer, PublicRelationsOfficer, Health;
-
-}
