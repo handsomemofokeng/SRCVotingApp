06b14a202eec6ce0d35bbf655e8d8fd65ad24c64
handsome.mofokeng <handsomem23@hotmail.com>
2019-10-14 01:27:45 +0200

    Added InsightsActivity

---
+++ app/src/main/java/com/example/srcvotingapp/InsightsActivity.java
@@ -0,0 +1,13 @@
+package com.example.srcvotingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsightsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insights);
+    }
+}
